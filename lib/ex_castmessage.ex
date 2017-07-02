15b3a801bc74822d796f5e52ac7a5134529feb80
Ryuichi Maeno <ryuone@gmail.com>
2017-07-02 21:05:18 +0900

    Initial.

---
+++ lib/ex_castmessage.ex
@@ -0,0 +1,18 @@
+defmodule ExCastmessage do
+  @moduledoc """
+  Documentation for ExCastmessage.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCastmessage.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_castmessage_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCastmessageTest do
+  use ExUnit.Case
+  doctest ExCastmessage
+
+  test "greets the world" do
+    assert ExCastmessage.hello() == :world
+  end
+end
